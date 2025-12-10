e6e69ac334d568c2dfe1fa10bc080ad1df1c941e
EdvinoT <edvinot82@gmail.com>
2025-12-10 14:21:06 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+num_list = list(range(1,101))
+for x in num_list:
+    if x%3 == 0:
+        print("Fizz")
+    elif 5%x ==0:
+        print('Buzz')
+    else:
+        3%x and 5%x
+        print("FizzBuzz")
 
+print(num_list)
